a20794a6f76bb1c06a6f46e0b55b9f7a87eca4ef
mike840609 <mike840609@gmail.com>
2016-03-16 01:02:42 +0800

    finish part 1

--- Photomania/Five100px.swift
+++ Photomania/Five100px.swift
@@ -8,130 +8,212 @@
 //
 
 import UIKit
+import Alamofire
+
+
+//創建自定義響應序列化方法 Serializer
+extension Alamofire.Request{
+
+    // 創建一個方法返回ResponseSerializer結構體,此結構體為Alamofire中處理序列化的主體,接收底層NSURLSession請求及實現NSData方法(伺服器傳來),來作為參數,該方法用這些對象來序列化
+    
+    class func imageResponseSerializer() -> ResponseSerializer<UIImage?,NSError> {
+        
+        return ResponseSerializer{request,response,data,error in
+            
+            guard error == nil else{return .Failure(error!)}
+            
+            guard let valiData = data else{
+                
+                let failureReason = "數據無法序列化,因為接收到的為空值"
+                let error = Error.errorWithCode(.DataSerializationFailed, failureReason: failureReason)
+                
+                return .Failure(error)
+                
+            }
+            let image = UIImage(data:valiData, scale: UIScreen.mainScreen().scale)
+            return .Success(image)
+            
+        }
+        
+    }
+    // completionHandler 一旦我們從伺服器序列化數據 程式碼便會運行,調用Alamofire中的response響應處理方法
+    public func responseImage(completionHandler: Response<UIImage?,NSError> -> Void) -> Self{
+        
+        return response(responseSerializer:Request.imageResponseSerializer(),completionHandler: completionHandler)
+    }
+}
+
 
 struct Five100px {
-  enum ImageSize: Int {
-    case Tiny = 1
-    case Small = 2
-    case Medium = 3
-    case Large = 4
-    case XLarge = 5
-  }
-  
-  enum Category: Int, CustomStringConvertible {
-    case Uncategorized = 0, Celebrities, Film, Journalism, Nude, BlackAndWhite, StillLife, People, Landscapes, CityAndArchitecture, Abstract, Animals, Macro, Travel, Fashion, Commercial, Concert, Sport, Nature, PerformingArts, Family, Street, Underwater, Food, FineArt, Wedding, Transportation, UrbanExploration
     
-    var description: String {
-      get {
-        switch self {
-        case .Uncategorized: return "Uncategorized"
-        case .Celebrities: return "Celebrities"
-        case .Film: return "Film"
-        case .Journalism: return "Journalism"
-        case .Nude: return "Nude"
-        case .BlackAndWhite: return "Black And White"
-        case .StillLife: return "Still Life"
-        case .People: return "People"
-        case .Landscapes: return "Landscapes"
-        case .CityAndArchitecture: return "City And Architecture"
-        case .Abstract: return "Abstract"
-        case .Animals: return "Animals"
-        case .Macro: return "Macro"
-        case .Travel: return "Travel"
-        case .Fashion: return "Fashion"
-        case .Commercial: return "Commercial"
-        case .Concert: return "Concert"
-        case .Sport: return "Sport"
-        case .Nature: return "Nature"
-        case .PerformingArts: return "Performing Arts"
-        case .Family: return "Family"
-        case .Street: return "Street"
-        case .Underwater: return "Underwater"
-        case .Food: return "Food"
-        case .FineArt: return "Fine Art"
-        case .Wedding: return "Wedding"
-        case .Transportation: return "Transportation"
-        case .UrbanExploration: return "Urban Exploration"
+    //  Urlstring 實作 遵循 URLRequestConvertible 的枚舉類型 (Alamofire 中定義),實作協定 必須要有一個名urlrequest 的 NSMutableURLRequest 變數
+    enum Router: URLRequestConvertible {
+        static let baseURLString = "https://api.500px.com/v1"
+        static let consumerKey = "iENlAc0OEHHLpExY8jBzpHi5wWepqAPtxHE8CXNO"
+        
+        case PopularPhotos(Int)
+        case PhotoInfo(Int, ImageSize)
+        case Comments(Int, Int)
+        
+        // 定義算值屬性,每次使用枚舉都會構造出特定case中需要的URL
+        
+        // Example
+        
+        // Five100px.Router.PhotoInfo(10000, Five100px.ImageSize.Large)
+        // URL: https://api.500px.com/v1/photos/10000?consumer_key=xxxxxx&image_size=4
+        //      https://api.500px.com/v1  +  /photos/10000  +  ?consumer_key=xxxxxx&image_size=4
+        //      baseURLString             +  path           +  encoded parameters
+        var URLRequest: NSMutableURLRequest {
+            let result: (path: String, parameters: [String: AnyObject]) = {
+                
+                switch self {
+                    
+                case .PopularPhotos(let page):
+                    let params = ["consumer_key": Router.consumerKey, "page": "\(page)", "feature": "popular", "rpp": "50",  "include_store": "store_download", "include_states": "votes"]
+                    return ("/photos", params)
+                    
+                case .PhotoInfo(let photoID, let imageSize):
+                    let params = ["consumer_key": Router.consumerKey, "image_size": "\(imageSize.rawValue)"]
+                    return ("/photos/\(photoID)", params)
+                    
+                case .Comments(let photoID, let commentsPage):
+                    let params = ["consumer_key": Router.consumerKey, "comments": "1", "comments_page": "\(commentsPage)"]
+                    return ("/photos/\(photoID)/comments", params)
+                }
+            }()
+            
+            let URL = NSURL(string: Router.baseURLString)!
+            let URLRequest = NSURLRequest(URL: URL.URLByAppendingPathComponent(result.path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            
+            return encoding.encode(URLRequest, parameters: result.parameters).0
         }
-      }
     }
-  }
+    
+    
+    
+    enum ImageSize: Int {
+        case Tiny = 1
+        case Small = 2
+        case Medium = 3
+        case Large = 4
+        case XLarge = 5
+    }
+    
+    enum Category: Int, CustomStringConvertible {
+        case Uncategorized = 0, Celebrities, Film, Journalism, Nude, BlackAndWhite, StillLife, People, Landscapes, CityAndArchitecture, Abstract, Animals, Macro, Travel, Fashion, Commercial, Concert, Sport, Nature, PerformingArts, Family, Street, Underwater, Food, FineArt, Wedding, Transportation, UrbanExploration
+        
+        var description: String {
+            get {
+                switch self {
+                case .Uncategorized: return "Uncategorized"
+                case .Celebrities: return "Celebrities"
+                case .Film: return "Film"
+                case .Journalism: return "Journalism"
+                case .Nude: return "Nude"
+                case .BlackAndWhite: return "Black And White"
+                case .StillLife: return "Still Life"
+                case .People: return "People"
+                case .Landscapes: return "Landscapes"
+                case .CityAndArchitecture: return "City And Architecture"
+                case .Abstract: return "Abstract"
+                case .Animals: return "Animals"
+                case .Macro: return "Macro"
+                case .Travel: return "Travel"
+                case .Fashion: return "Fashion"
+                case .Commercial: return "Commercial"
+                case .Concert: return "Concert"
+                case .Sport: return "Sport"
+                case .Nature: return "Nature"
+                case .PerformingArts: return "Performing Arts"
+                case .Family: return "Family"
+                case .Street: return "Street"
+                case .Underwater: return "Underwater"
+                case .Food: return "Food"
+                case .FineArt: return "Fine Art"
+                case .Wedding: return "Wedding"
+                case .Transportation: return "Transportation"
+                case .UrbanExploration: return "Urban Exploration"
+                }
+            }
+        }
+    }
 }
 
 class PhotoInfo: NSObject {
-  let id: Int
-  let url: String
-  
-  var name: String?
-  
-  var favoritesCount: Int?
-  var votesCount: Int?
-  var commentsCount: Int?
-  
-  var highest: Float?
-  var pulse: Float?
-  var views: Int?
-  var camera: String?
-  var focalLength: String?
-  var shutterSpeed: String?
-  var aperture: String?
-  var iso: String?
-  var category: Five100px.Category?
-  var taken: String?
-  var uploaded: String?
-  var desc: String?
-  
-  var username: String?
-  var fullname: String?
-  var userPictureURL: String?
-  
-  init(id: Int, url: String) {
-    self.id = id
-    self.url = url
-  }
-  
-  required init(response: NSHTTPURLResponse, representation: AnyObject) {
-    self.id = representation.valueForKeyPath("photo.id") as! Int
-    self.url = representation.valueForKeyPath("photo.image_url") as! String
+    let id: Int
+    let url: String
     
-    self.favoritesCount = representation.valueForKeyPath("photo.favorites_count") as? Int
-    self.votesCount = representation.valueForKeyPath("photo.votes_count") as? Int
-    self.commentsCount = representation.valueForKeyPath("photo.comments_count") as? Int
-    self.highest = representation.valueForKeyPath("photo.highest_rating") as? Float
-    self.pulse = representation.valueForKeyPath("photo.rating") as? Float
-    self.views = representation.valueForKeyPath("photo.times_viewed") as? Int
-    self.camera = representation.valueForKeyPath("photo.camera") as? String
-    self.focalLength = representation.valueForKeyPath("photo.focal_length") as? String
-    self.shutterSpeed = representation.valueForKeyPath("photo.shutter_speed") as? String
-    self.aperture = representation.valueForKeyPath("photo.aperture") as? String
-    self.iso = representation.valueForKeyPath("photo.iso") as? String
-    self.taken = representation.valueForKeyPath("photo.taken_at") as? String
-    self.uploaded = representation.valueForKeyPath("photo.created_at") as? String
-    self.desc = representation.valueForKeyPath("photo.description") as? String
-    self.name = representation.valueForKeyPath("photo.name") as? String
+    var name: String?
     
-    self.username = representation.valueForKeyPath("photo.user.username") as? String
-    self.fullname = representation.valueForKeyPath("photo.user.fullname") as? String
-    self.userPictureURL = representation.valueForKeyPath("photo.user.userpic_url") as? String
-  }
-  
-  override func isEqual(object: AnyObject!) -> Bool {
-    return (object as! PhotoInfo).id == self.id
-  }
-  
-  override var hash: Int {
-    return (self as PhotoInfo).id
-  }
+    var favoritesCount: Int?
+    var votesCount: Int?
+    var commentsCount: Int?
+    
+    var highest: Float?
+    var pulse: Float?
+    var views: Int?
+    var camera: String?
+    var focalLength: String?
+    var shutterSpeed: String?
+    var aperture: String?
+    var iso: String?
+    var category: Five100px.Category?
+    var taken: String?
+    var uploaded: String?
+    var desc: String?
+    
+    var username: String?
+    var fullname: String?
+    var userPictureURL: String?
+    
+    init(id: Int, url: String) {
+        self.id = id
+        self.url = url
+    }
+    
+    required init(response: NSHTTPURLResponse, representation: AnyObject) {
+        self.id = representation.valueForKeyPath("photo.id") as! Int
+        self.url = representation.valueForKeyPath("photo.image_url") as! String
+        
+        self.favoritesCount = representation.valueForKeyPath("photo.favorites_count") as? Int
+        self.votesCount = representation.valueForKeyPath("photo.votes_count") as? Int
+        self.commentsCount = representation.valueForKeyPath("photo.comments_count") as? Int
+        self.highest = representation.valueForKeyPath("photo.highest_rating") as? Float
+        self.pulse = representation.valueForKeyPath("photo.rating") as? Float
+        self.views = representation.valueForKeyPath("photo.times_viewed") as? Int
+        self.camera = representation.valueForKeyPath("photo.camera") as? String
+        self.focalLength = representation.valueForKeyPath("photo.focal_length") as? String
+        self.shutterSpeed = representation.valueForKeyPath("photo.shutter_speed") as? String
+        self.aperture = representation.valueForKeyPath("photo.aperture") as? String
+        self.iso = representation.valueForKeyPath("photo.iso") as? String
+        self.taken = representation.valueForKeyPath("photo.taken_at") as? String
+        self.uploaded = representation.valueForKeyPath("photo.created_at") as? String
+        self.desc = representation.valueForKeyPath("photo.description") as? String
+        self.name = representation.valueForKeyPath("photo.name") as? String
+        
+        self.username = representation.valueForKeyPath("photo.user.username") as? String
+        self.fullname = representation.valueForKeyPath("photo.user.fullname") as? String
+        self.userPictureURL = representation.valueForKeyPath("photo.user.userpic_url") as? String
+    }
+    // 改寫 isEqual 以及 hash 方法
+    override func isEqual(object: AnyObject!) -> Bool {
+        return (object as! PhotoInfo).id == self.id
+    }
+    
+    override var hash: Int {
+        return (self as PhotoInfo).id
+    }
 }
 
 class Comment {
-  let userFullname: String
-  let userPictureURL: String
-  let commentBody: String
-  
-  init(JSON: AnyObject) {
-    userFullname = JSON.valueForKeyPath("user.fullname") as! String
-    userPictureURL = JSON.valueForKeyPath("user.userpic_url") as! String
-    commentBody = JSON.valueForKeyPath("body") as! String
-  }
+    let userFullname: String
+    let userPictureURL: String
+    let commentBody: String
+    
+    init(JSON: AnyObject) {
+        userFullname = JSON.valueForKeyPath("user.fullname") as! String
+        userPictureURL = JSON.valueForKeyPath("user.userpic_url") as! String
+        commentBody = JSON.valueForKeyPath("body") as! String
+    }
 }
--- Photomania/PhotoBrowserCollectionViewController.swift
+++ Photomania/PhotoBrowserCollectionViewController.swift
@@ -7,116 +7,215 @@
 //
 
 import UIKit
+import Alamofire
 
 class PhotoBrowserCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-  var photos = NSMutableOrderedSet()
-  
-  let refreshControl = UIRefreshControl()
-  
-  let PhotoBrowserCellIdentifier = "PhotoBrowserCell"
-  let PhotoBrowserFooterViewIdentifier = "PhotoBrowserFooterView"
-  
-  // MARK: Life-cycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    setupView()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
-  // MARK: CollectionView
-  
-  override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return photos.count
-  }
-  
-  override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PhotoBrowserCellIdentifier, forIndexPath: indexPath) as! PhotoBrowserCollectionViewCell
+    //無序、元素不重複
+    var photos = Set<PhotoInfo>()
     
-    return cell
-  }
-  
-  override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-    return collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: PhotoBrowserFooterViewIdentifier, forIndexPath: indexPath) as UICollectionReusableView
-  }
-  
-  override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-    performSegueWithIdentifier("ShowPhoto", sender: (self.photos.objectAtIndex(indexPath.item) as! PhotoInfo).id)
-  }
-  
-  // MARK: Helper
-  
-  func setupView() {
-    navigationController?.setNavigationBarHidden(false, animated: true)
+    let refreshControl = UIRefreshControl()
     
-    let layout = UICollectionViewFlowLayout()
-    let itemWidth = (view.bounds.size.width - 2) / 3
-    layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
-    layout.minimumInteritemSpacing = 1.0
-    layout.minimumLineSpacing = 1.0
-    layout.footerReferenceSize = CGSize(width: collectionView!.bounds.size.width, height: 100.0)
+    // 紀錄是否更新照片 以及正在瀏覽的頁面
+    var populatingPhotos = false
+    var currentPage = 1
     
-    collectionView!.collectionViewLayout = layout
+    let PhotoBrowserCellIdentifier = "PhotoBrowserCell"
+    let PhotoBrowserFooterViewIdentifier = "PhotoBrowserFooterView"
     
-    let titleLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 60.0, height: 30.0))
-    titleLabel.text = "Photomania"
-    titleLabel.textColor = UIColor.whiteColor()
-    titleLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
-    navigationItem.titleView = titleLabel
+    // MARK: Life-cycle
     
-    collectionView?.registerClass(PhotoBrowserCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: PhotoBrowserCellIdentifier)
-    collectionView?.registerClass(PhotoBrowserCollectionViewLoadingCell.classForCoder(), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: PhotoBrowserFooterViewIdentifier)
-    
-    refreshControl.tintColor = UIColor.whiteColor()
-    refreshControl.addTarget(self, action: "handleRefresh", forControlEvents: .ValueChanged)
-    collectionView!.addSubview(refreshControl)
-  }
-  
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "ShowPhoto" {
-      (segue.destinationViewController as! PhotoViewerViewController).photoID = sender!.integerValue
-      (segue.destinationViewController as! PhotoViewerViewController).hidesBottomBarWhenPushed = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        
+        populatePhotos()
+        
+        
+        
     }
-  }
-  
-  func handleRefresh() {
     
-  }
+    
+    // 1.滾動超過80% 載入圖片
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        if scrollView.contentOffset.y + view.frame.size.height > scrollView.contentSize.height * 0.8{
+            populatePhotos()
+        }
+    }
+    
+    func populatePhotos(){
+        
+        // 2.populatePhotos()方法在currentPage中載入 使用 populatingPhotos 當作標記防止還在載入頁面時載入下一個頁面
+        if populatingPhotos {
+            return
+        }
+        
+        populatingPhotos = true
+
+        
+        // 3.使用創建的路由,只需要將頁數傳遞過去,將返回該頁面的URL字串,api每次大約返回50張,為下一批顯示的照片需要重新呼叫路由
+        Alamofire.request(Five100px.Router.PopularPhotos(self.currentPage)).responseJSON { (response) -> Void in
+            
+            func failed(){self.populatingPhotos = false}
+            
+            guard let JSON = response.result.value else{failed(); return}
+            
+            if response.result.error != nil {failed() ; return}
+            
+            // 4. .responseJSON 後面的程式碼 completion handler(完成處理方法)必須在主線成上面運行,若有其他工作在運行需要用GCD調用到其他列隊,這裡用DISPATCH_QUEUE_PRIORITY_HIGH,來運行操作
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)){
+                
+                // 5.將JSON數據中的 photos 對應的 value 取出
+                guard let photoJsons = JSON.valueForKey("photos") as? [NSDictionary] else {return}
+                
+                // 6.再添加新的照片時儲存圖片當前的數量
+                let lastItem = self.photos.count
+                
+                // 7.foreach 函數去run photosJsons 字典數組 , 篩選掉nsfw(not safe dor work)圖片,並將 photoinfo(five500.px中定義) 加到 photos 集合中,自定義的類別中改寫 isEqual 以及 hash 方法,這兩個方法都用 id 來比較,因此排序和唯一話photoinfo 對象仍是一個較快的操作
+                photoJsons.forEach{
+                    
+                    guard let nsfw = $0["nsfw"] as? Bool,
+                        let id = $0["id"] as? Int,
+                        let url = $0["image_url"] as? String
+                        where nsfw == false else { return }
+                    
+                    // 8.如果有人上傳新圖片,所獲得新的一批照片可能包含部分已經下載的照片,這就是為何定義集合Set<photoinfo>,集合內的項目必須要唯一,重複圖片不再次出現
+                    self.photos.insert(PhotoInfo(id: id, url:url))
+                }
+                
+                // 9.創建一個 NSIndexPath 是數組 並將其插入到collectionView
+                let indexPaths = (lastItem..<self.photos.count).map {NSIndexPath(forItem: $0, inSection: 0)}
+                
+                // 10.在主執行緒上將集合試圖插入項目,因為UIKit操作都要在主執行緒上
+                dispatch_async(dispatch_get_main_queue()){
+                    self.collectionView!.insertItemsAtIndexPaths(indexPaths)
+                }
+                
+                self.currentPage++
+            }
+            
+        }
+        
+        self.populatingPhotos = false
+    }
+    
+    // MARK: CollectionView
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PhotoBrowserCellIdentifier, forIndexPath: indexPath) as! PhotoBrowserCollectionViewCell
+        
+        
+        
+        // 設定Cell
+        let imageURL = self.photos[self.photos.startIndex.advancedBy(indexPath.item)].url
+        cell.imageView.image = nil
+        cell.request?.cancel()
+        
+        cell.request = Alamofire.request(.GET,imageURL).responseImage{
+            
+            response in
+            
+            guard let image = response.result.value where response.result.error == nil else{return}
+            
+            cell.imageView.image = image
+            
+        }
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: PhotoBrowserFooterViewIdentifier, forIndexPath: indexPath) as UICollectionReusableView
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        performSegueWithIdentifier("ShowPhoto", sender: self.photos[self.photos.startIndex.advancedBy(indexPath.item)].id)
+    }
+    
+    // MARK: Helper
+    
+    func setupView() {
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        let layout = UICollectionViewFlowLayout()
+        let itemWidth = (view.bounds.size.width - 2) / 3
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth)
+        layout.minimumInteritemSpacing = 1.0
+        layout.minimumLineSpacing = 1.0
+        layout.footerReferenceSize = CGSize(width: collectionView!.bounds.size.width, height: 100.0)
+        
+        collectionView!.collectionViewLayout = layout
+        
+        let titleLabel = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 60.0, height: 30.0))
+        titleLabel.text = "Photomania"
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        navigationItem.titleView = titleLabel
+        
+        collectionView?.registerClass(PhotoBrowserCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: PhotoBrowserCellIdentifier)
+        collectionView?.registerClass(PhotoBrowserCollectionViewLoadingCell.classForCoder(), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: PhotoBrowserFooterViewIdentifier)
+        
+        refreshControl.tintColor = UIColor.whiteColor()
+        refreshControl.addTarget(self, action: "handleRefresh", forControlEvents: .ValueChanged)
+        collectionView!.addSubview(refreshControl)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowPhoto" {
+            (segue.destinationViewController as! PhotoViewerViewController).photoID = sender!.integerValue
+            (segue.destinationViewController as! PhotoViewerViewController).hidesBottomBarWhenPushed = true
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func handleRefresh() {
+        
+    }
 }
 
 class PhotoBrowserCollectionViewCell: UICollectionViewCell {
-  let imageView = UIImageView()
-  
-  required init(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)!
-  }
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
     
-    backgroundColor = UIColor(white: 0.1, alpha: 1.0)
+    let imageView = UIImageView()
+    var request:Alamofire.Request?      //用此屬性來儲存Alamofire得請求來載入圖片
     
-    imageView.frame = bounds
-    addSubview(imageView)
-  }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor(white: 0.1, alpha: 1.0)
+        
+        imageView.frame = bounds
+        addSubview(imageView)
+    }
 }
 
 class PhotoBrowserCollectionViewLoadingCell: UICollectionReusableView {
-  let spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
-  
-  required init(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)!
-  }
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.WhiteLarge)
     
-    spinner.startAnimating()
-    spinner.center = self.center
-    addSubview(spinner)
-  }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        spinner.startAnimating()
+        spinner.center = self.center
+        addSubview(spinner)
+    }
 }
